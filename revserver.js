2cc33ff7259ad39c1cbf14a3ad9d5e268db95d47
李明光 <30451103+PurpleVeil@users.noreply.github.com>
2017-07-27 00:58:12 +1000

    commit1111

--- revserver.js
+++ revserver.js
@@ -8,8 +8,11 @@ var multer = require('multer');
 var bodyParser = require('body-parser');
 var path =require('path');
 var app = Express();
+var fs = require("fs");
+var unzip = require("unzip2");
 app.use(bodyParser.json());
 app.use(Express.static(path.join(__dirname, 'Images')));
+app.use(Express.static(path.join(__dirname, 'zip')));
 var url;
 var Storage = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -29,8 +32,81 @@ app.post("/", function (req, res) {
 		if (err) {
 			return res.end("Something went wrong!");
 		}
+		if (url.substring(url.length-3,url.length)!="zip")
         res.send("<a href="+url+">" +url+ "</a >");
+		else {
+            function deleteall(path) {
+                var files = [];
+                if (fs.existsSync(path)) {
+                    files = fs.readdirSync(path);
+                    files.forEach(function (file, index) {
+                        var curPath = path + "/" + file;
+                        if (fs.statSync(curPath).isDirectory()) { // recurse
+                            deleteall(curPath);
+                        } else { // delete file
+                            fs.unlinkSync(curPath);
+                        }
+                    });
+                    fs.rmdirSync(path);
+                }
+                // clean zip dir
+                deleteall("./zip")
+                var extract = unzip.Extract({path: './zip'});
+                extract.on('error', function (err) {
+                    console.log("error++++++++++++++++++++++");
+                    console.log(err);
+                    //解压异常处理
+                });
+                extract.on('finish', function () {
+                    console.log("解压完成!!");
+                    //解压完成处理
+                });
+                fs.createReadStream("./Images/" + url).pipe(extract);
+            }
+var path1="";
+            function explorer(path) {
+                fs.readdir(path, function (err, files) {
+                    //err 为错误 , files 文件名列表包含文件夹与文件
+                    if (err) {
+                        console.log('error:\n' + err);
+                        return;
+                    }
+                    files.forEach(function (file) {
 
+                        fs.stat(path + '/' + file, function (err, stat) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            if (stat.isDirectory()) {
+                                // 如果是文件夹遍历
+                                explorer(path + '/' + file);
+                            } else {
+                                // 读出所有的文件
+                                console.log(path.substring(5,path.length));
+                                if (path.substring(5,path.length)!="/__MACOSX")
+                                path1=path1+"<a href="+path.substring(5,path.length) + '/' + file+">" +
+                                    path.substring(5,path.length) + '/' + file+ "</a >"+"\n";
+                                //console.log(path1)
+                            }
+                        });
+
+                    });
+                });
+            }
+
+            var interval = setInterval(function () {
+                explorer("./zip")
+                //console.log(path1)
+               // res.send(path1)
+                clearInterval(interval);
+            }, 2000);
+            var interval1 = setInterval(function () {
+                res.send(path1)
+                clearInterval(interval1);
+            }, 2500);
+
+        }
     });
 });
 const http = require('http'); 　//实例化“http”
@@ -38,7 +114,7 @@ const http = require('http'); 　//实例化“http”
 const hostname = '127.0.0.1';
 const port = 3000;
 app.listen(port, hostname, function () {
-	  console.log('Revision app listening on port 3000!')
+	  console.log('app listening on port 3000!')
 	})
 
 module.exports = app;
